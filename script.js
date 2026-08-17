58eeed673be1a34882974b081146387df925ef4b
Kingori-software <its.kingori.dev@gmail.com>
2026-08-17 09:08:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ const events = [
 
 // ---------- MEDIA TEAM DATA ----------
 const mediaTeam = [
-  { David: "Media Person 1", role: "Sound" },
+  { name: "David", role: "Sound" },
   { name: "Media Person 2", role: "Projection" },
   { name: "Media Person 3", role: "Photography" },
   { name: "Media Person 4", role: "Stage Management" }
