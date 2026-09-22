dd25c198639c105c1b2c48f3a6a8c361f4845c67
Kingori-software <its.kingori.dev@gmail.com>
2026-09-22 20:27:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -22,19 +22,19 @@ image: "https://i.ibb.co/d0SQskDQ/IMG-20260906-WA7380.jpg"
 
 // ---------- MEDIA TEAM DATA (Two separate arrays) ----------
 const firstTeam = [
-  { name: "Frank", role: "Sound" },
-  { name: "David", role: "Projection" },
-  { name: "Florence", role: "Photography" },
-  { name: "Mark", role: "Cable and Stage Management" },
-  { name: "Loftus", role: "Note taking" }
+  { name: "Blank", role: "Sound" },
+  { name: "Blank", role: "Projection" },
+  { name: "Blank", role: "Photography" },
+  { name: "Blank", role: "Cable and Stage Management" },
+  { name: "Blank", role: "Note taking" }
 ];
 
 const secondTeam = [
-  { name: "Frank", role: "Sound" },
-  { name: "David", role: "Projections" },
-  { name: "Florence", role: "Socials" },
-  { name: "Florence", role: "Photography" },
-  { name: "Mark", role: "Cable and Stage management" }
+  { name: "Blank", role: "Sound" },
+  { name: "Blank", role: "Projections" },
+  { name: "Blank", role: "Socials" },
+  { name: "Blank", role: "Photography" },
+  { name: "Blank", role: "Cable and Stage management" }
 ];
 
 // ---------- BOT ANSWERS ----------
